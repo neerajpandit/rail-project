8bbeee1dc8a89c3cf767b0b50482cd988a100285
Neerajpandey1507 <pandeyneeraj1507@gmail.com>
2022-12-19 16:24:32 +0530

    second2 commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from .import views
 urlpatterns = [
-    path('',views.HomeView.as_view(),name='home')
+    path('/',views.HomeView.as_view(),name='home'),
+    path('',views.HomeView.as_view(),name='home'),
+    path('paper/',views.PaperView.as_view(),name='paper'),
+    path('notes/',views.NotesView.as_view(),name='notes'),
+    path('syllabus/',views.SyllabusView.as_view(),name='syllabus'),
+    path('timetable/',views.TimetableView.as_view(),name='timetable'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,65 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from .models import Branch,Semester,Paper,Notes,Syllabus,Timetable
 # Create your views here.
 class HomeView(ListView):
     def get(self,request):
         return render(request,'myapp/home.html')
+
+
+class HomeView(ListView):
+    def get(self,request):
+        return render(request,'myapp/index.html')
+
+class PaperView(ListView):
+    def get(self,request):
+        branch=self.request.GET.get('branch')
+        sem=self.request.GET.get('sem')
+        year=self.request.GET.get('year')
+        result=Paper.objects.filter(
+            sem__sem=sem,
+            branch__branch=branch,
+            year=year
+        )
+
+        opsem=Semester.objects.all()
+        opbranch=Branch.objects.all()
+        opyear=Paper.objects.all()
+        return render(request,'myapp/paper.html',{'opsem':opsem,'opbranch':opbranch,'opyear':opyear,'result':result})
+
+class NotesView(ListView):
+    def get(self,request):
+        branch=self.request.GET.get('branch')
+        sem=self.request.GET.get('sem')
+        notesresult=Notes.objects.filter(
+            sem__sem=sem,
+            branch__branch=branch
+        )
+        opsem=Semester.objects.all()
+        opbranch=Branch.objects.all()
+        return render(request,'myapp/notes.html',{'notesresult':notesresult,'opsem':opsem,'opbranch':opbranch})
+
+class SyllabusView(ListView):
+    def get(self,request):
+        branch=self.request.GET.get('branch')
+        sem=self.request.GET.get('sem')
+        syllabusresult=Syllabus.objects.filter(
+            sem__sem=sem,
+            branch__branch=branch
+        )
+        opsem=Semester.objects.all()
+        opbranch=Branch.objects.all()
+        return render(request,'myapp/syllabus.html',{'syllabusresult':syllabusresult,'opsem':opsem,'opbranch':opbranch})
+
+class TimetableView(ListView):
+    def get(self,request):
+        branch=self.request.GET.get('branch')
+        sem=self.request.GET.get('sem')
+        timetableresult=Timetable.objects.filter(
+            sem__sem=sem,
+            branch__branch=branch
+        )
+        opsem=Semester.objects.all()
+        opbranch=Branch.objects.all()
+        return render(request,'myapp/timetable.html',{'timetableresult':timetableresult,'opsem':opsem,'opbranch':opbranch})
+ 
